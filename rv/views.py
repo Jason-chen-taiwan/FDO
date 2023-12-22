eadc8ea1547c88d414426ee66fb66200e5ff01f9
Jason-chen-taiwan <zwasd5123@gmail.com>
2023-12-22 08:52:20 +0800

    owner view add hadn't add url and model

--- rv/views.py
+++ rv/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 
 def rv_view(request):
     return render(request, "rendezvous.html")
+
+def to0_get(request):
+    if request.method == 'POST':
+        guid = request.POST.get('GUID')
+        owner_name = request.POST.get('clientusername')
